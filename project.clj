6b1e01d40f5f264fcdd11cffa694cb3796af900a
Thomas Reynolds <me@tdreyno.com>
2014-01-27 11:36:54 -0800

    move code to src

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject tesselax "0.0.1"
-  :description "The page routing ring handler for caribou"
+  :description "Automatic animated layouts in clojurescript"
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 ;; [org.clojars.magomimmo/core.async "0.1.0-SNAPSHOT"]
                  [domina "1.0.2"]
                  [ring "1.2.0"]
                  [org.clojure/google-closure-library-third-party "0.0-2029"]]
@@ -15,17 +14,24 @@
   :resource-paths ["resources/"]
   :min-lein-version "2.0.0"
   :migration-namespace tesselax.migrations
-  :main tesselax.core
-  :ring {:handler tesselax.core/handler
-         :init tesselax.core/init
+  :main tesselax.server
+  :ring {:handler tesselax.server/handler
+         :init tesselax.server/init
          :port 12321
          :auto-reload? false
          :servlet-name "tesselax-frontend"}
   :cljsbuild {
     :builds {
       :dev {
-        :source-paths ["resources/cljs"]  
+        :source-paths ["src"]  
         :compiler {
-          :optimizations :whitespace
-          :output-to  "resources/public/js/app/tesselax.js"
-          :output-dir "resources/public/js/app/out"}}}})
+          :optimizations :none
+          :output-to "resources/public/js/app/tesselax.js"
+          :output-dir "resources/public/js/app/out"
+          :source-map true}}
+       :prod {
+        :source-paths ["src"]
+        :compiler {
+          :output-to "resources/public/js/tesselax.min.js"
+          :optimizations :advanced
+          :pretty-print false}}}})
